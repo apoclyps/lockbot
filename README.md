c6fb6d2cf02d0002ed795a973dedfdb172a01403
Kyle Harrison <kyle90adam@hotmail.com>
2019-03-06 20:07:49 +0000

    Adding simple lockbot logic

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Lockbot
-A project that uses the [slack-machine](https://slack-machine.readthedocs.io/) package and the [Slack Developer Kit for Python](https://slackapi.github.io/python-slackclient/) to provide a customisable Slack bot for managing deployments.
+
+A project that uses the [slack-machine](https://slack-machine.readthedocs.io/) package and the [Slack Developer Kit for Python](https://slackapi.github.io/python-slackclient/) to provide a customisable Slack bot for managing lockments.
 
 <p align="center">
   <img src="https://i.ytimg.com/vi/U2TQyuxzWRA/mqdefault.jpg" alt="lenny" height="240" width="320" />
 </p>
 
-## Quickstart
+### Quickstart
+
 After you clone this repository, you can provide your environment configuration within a `.env` file located in the root directory. The following variables are required:
 
 ```bash
@@ -21,3 +23,25 @@ To run the slack machine, you can build the docker container and run the slack m
 docker-compose build bot
 docker-compose run bot
 ```
+
+### Commands
+
+#### help
+
+Display's all available commands listed below:
+
+### list services
+
+Show a list of all known services.
+
+### lock `service-name`
+
+Lock the service.
+
+### release `service-name`
+
+Release the deployments locks for the service.
+
+### Contributing
+
+Pull requests on this bot are very welcome. Please read CONTRIBUTING.md for details on the code of conduct, and the process for submitting pull requests.
--- plugins/lockbot.py
+++ plugins/lockbot.py
@@ -13,14 +13,37 @@ storage = RedisStorage(settings)
 class DeploymentPlugin(MachineBasePlugin):
     """Deployment"""
 
-    @respond_to(r"^lock$")
-    def lock(self, msg):
-        """lock: spread the lock"""
-        storage.set("services", "locked")
-        msg.say("Serviced locked")
+    @respond_to(r"^list$")
+    @respond_to(r"^list services$")
+    def list(self, msg):
+        """list services: list all registered services"""
+        services = [key.decode("utf-8") for key in storage._redis.keys()]
 
-    @respond_to(r"^release$")
-    def release(self, msg):
-        """release: spread the release"""
-        storage.set("services", "locked")
-        msg.say("Serviced released")
+        response = "All Services:\n\n"
+        for service in services:
+            service = service.replace("SM:", "")
+            response += f"* {service}\n\n"
+
+        msg.say(response)
+
+    @respond_to(r"^lock (?P<service>.*)$")
+    def lock(self, msg, service):
+        """lock <service>: lock a given service"""
+        state = storage.get(service).decode("utf-8")
+
+        if state == "locked":
+            msg.say(f"Service {service} is already locked :lock:")
+        else:
+            storage.set(service, "locked")
+            msg.say(f"Service {service} locked :lock:")
+
+    @respond_to(r"^release (?P<service>.*)$")
+    def release(self, msg, service):
+        """release <service_name>: release a given service"""
+        state = storage.get(service).decode("utf-8")
+
+        if state == "locked":
+            storage.set(service, "released")
+            msg.say(f"Service {service} is now available to deploy :white_check_mark:")
+        else:
+            msg.say(f"Service {service} is already released :white_check_mark:")
